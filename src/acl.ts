20a7ec34319a606257cada58deb8944e988b82fb
D10221 <danieliozzi@gmail.com>
2016-06-27 07:07:30 +0200

    test passing

--- src/acl.ts
+++ src/acl.ts
@@ -1,24 +1,27 @@
 import * as Koa from 'koa';
 import * as pathToRegexp from 'path-to-regexp';
 import Options = pathToRegexp.Options;
-
-
-export interface Entry {
-    re: RegExp,
-    inclusiveList: any[]
-}
+const Debug = require('debug');
+const debug = Debug('koa-tiny-acl');
 
 export class Acl {
 
-    private _entries: Map<string, Entry> = new Map<string, Entry>();
+    private static _instance: Acl;
+    static get default(): Acl {
+        return Acl._instance || (Acl._instance = new Acl());
+    }
+
+    constructor(private _entries?: Map<string, Entry>) {
+        this._entries = this._entries || new Map<string, Entry>();
+    }
 
     /**
-     * @returns restriction fro patching path 
+     * @returns restriction from matching path
      */
     restrictions(url): string[] {
         if (!url) return null;
         for (let e of this._entries.values()) {
-            if (e.re.exec(url)) {
+            if (e.re.test(url)) {
                 return e.inclusiveList;
             }
         }
@@ -26,63 +29,75 @@ export class Acl {
     }
 
     /**
-     * restrict access to path , to provided list 
+     * restrict access to path , to provided list
      */
     restrict = (path: string, inclusiveList: string[], opts?: Options) => {
         this._entries.set(path, {
             re: pathToRegexp(path, opts),
             inclusiveList: inclusiveList
         });
+    }    
+
+    middleware = <T>(target: (ctx: Koa.Context) => T, claims: (x: T) => any[]): any /*Koa.Middleware*/ => {
+        
+        let self = this;
+
+        function find<T>(restrictions: T[], claims: T[]): boolean {
+            for (let restriction of restrictions) {
+                if (claims.find(r => r == restriction)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        let setup = (ctx): AclContext => {
+            ctx.acl = ctx.acl || self;
+            return ctx;
+        }
+
+        let hasAccess = (ctx: AclContext) => {
+
+            let restrictions = ctx && ctx.acl ? ctx.acl.restrictions(ctx.url) : null;
+            if (!restrictions) {
+                return true;
+            }
+
+            let subject = target(ctx);
+            if (!subject) {
+                return false;
+            }
+            let _claims = claims(subject);
+            const ok = find(restrictions, _claims);
+            if(!ok){
+                let restrictions = ctx.acl.restrictions(ctx.path).join(',');                        
+                console.log(`access denied to subject: ${`${subject ? JSON.stringify(subject): 'none'} \n`} with claims:[${_claims ? _claims.join(','): 'none'}] for path: ${ctx.path}, with restrictions: ${restrictions}`)
+            }
+            return ok;            
+        }
+
+        return function (ctx, next) :void {
+            if (hasAccess(setup(ctx))) {
+                next();
+                return;
+            }            
+            //Can't throw ! why ?
+            self.deny(ctx, next);
+        }
+    };
+
+    deny = (ctx: AclContext, next?): any => {        
+        ctx.status = 403;
     }
 }
-
-//default instance 
-let acl: Acl = new Acl;
-
-export const restrict = acl.restrict;
 
 export interface AclContext extends Koa.Context {
     acl?: Acl
 }
 
-export function middleware<T>(target: (ctx: Koa.Context) => T, claims: (x: T) => any[]): any /*Koa.Middleware*/ {
-
-    function find<T>(restrictions: T[], found: T[]) :boolean {
-        for (let restriction of restrictions) {
-            if (found.find(r => r == restriction)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    let setup = (ctx): AclContext => {
-        ctx.acl = ctx.acl || acl;
-        return ctx;
-    }
-
-    let hasAccess = (ctx: AclContext) => {
-
-        let restrictions = ctx && ctx.acl ? ctx.acl.restrictions(ctx.url) : null;
-        if (!restrictions) return true;
-
-        let subject = target(ctx);
-        if (!subject) return false;
-
-        return find(restrictions, claims(subject));;
-    }
-
-    return async function (ctx, next) {
-                       
-        if (hasAccess(setup(ctx))) {
-            next();
-        } else {
-            //Can't throw ! why ? 
-            return deny(ctx, next);
-        }
-    }
+export interface Entry {
+    re: RegExp,
+    inclusiveList: any[]
 }
 
-export let deny = (ctx:Koa.Context, next?) : any => {
-    ctx.status = 403;
-}
+ 
--- src/acl_test.ts
+++ src/acl_test.ts
@@ -1,7 +1,8 @@
 import {assert} from 'chai';
 import * as request from 'supertest';
 import * as Koa from 'koa';
-import * as acl from './acl';
+import {Acl} from './acl';
+import * as pathToRegexp from 'path-to-regexp';
 import * as router from 'koa-route-ts';
 
 // Users repo 
@@ -24,28 +25,27 @@ function authorize(credentials: string): string {
     return `Basic ${new Buffer(credentials).toString('Base64')}`;
 }
 
-function auth(getUser: (name:string, pass:string)=> any ) : Koa.Middleware {
-    
-    let regex = /Basic\s+(.*)/i;    
-    
-    return async function(ctx, next)  {                
-        
-        let r =  regex.exec(ctx.headers['authentication']);
-        if(!r) ctx.throw(401);
+function auth(getUser: (name: string, pass: string) => any): KoaMiddleware {
 
-        let auth =   new Buffer(r[1], 'base64').toString();    
-        if(!auth) ctx.throw(401);
-        
+    let regex = /Basic\s+(.*)/i;
+
+    return async function (ctx, next) {
+
+        let r = regex.exec(ctx.headers['authentication']);
+        if (!r) ctx.throw(401);
+
+        let auth = new Buffer(r[1], 'base64').toString();
+        if (!auth) ctx.throw(401);
+
         let parts = /^([^:]*):(.*)$/.exec(auth);
-                             
-        let user = getUser(parts[1], parts[2]);
-        if(!user) ctx.throw(401);
 
-        (ctx.request as any).user = user;        
+        let user = getUser(parts[1], parts[2]);
+        if (!user) ctx.throw(401);
+
+        (ctx.request as any).user = user;
         next();
     }
 }
-
 
 //User definition 
 export interface User {
@@ -59,43 +59,80 @@ let getUser = (ctx): User => {
     return ctx ? ctx.request.user : null
 }
 
+describe('restrictions', () => {
+
+    it('finds right restriction', () => {
+        const acl = new Acl();
+        acl.restrict('/a/:x*', ['x']);
+        let x = acl.restrictions('/');
+        assert.isNull(x);
+        x = acl.restrictions('a');
+        assert.isNull(x);
+        x = acl.restrictions('/a');
+        assert.deepEqual(x, ['x']);
+        x = acl.restrictions('/a/?');
+        assert.deepEqual(x, ['x']);
+    })
+})
+
 describe('Restrict Access', () => {
 
-    let app:Koa ; 
+    let app = new Koa();
+    let acl = Acl.default;
 
-    beforeEach(() => {
-        
-        app = new Koa();
+    //No Auth , no restriction 
+    app.use(router.get('/dmz', function (args) {
+        this.body = 'ok';
+    }))
 
-        // restrict user exists is users, sets ctx.user
-        app.use(auth(findUser));
+    // restrict user exists is users, sets ctx.user
+    app.use(auth(findUser));
 
-        // restrict user in 'role'
-        app.use(acl.middleware(getUser, user => user.roles));
+    // restrict user in 'role'
+    app.use(acl.middleware(getUser, user => user.roles));
 
-        //@restrict('admin')
-        acl.restrict('/:name', ['admin']);
-        let route = router.get('/:name', async function (name, next: Koa.Next) {
-            let ctx: Koa.Context = this;
-            ctx.status = 200;
-            ctx.body = 'ok';
-        })
-        app.use(route);
-    });
+    //@restrict('admin')
+    acl.restrict('/users/:name*', ['admin']);
+    app.use(router.get('/users/:name*', async function (name, next: KoaNext) {
+        let ctx: Koa.Context = this;
+        ctx.status = 200;
+        ctx.body = name;
+    }));
 
+    //Auth but no restriction
+    app.use(router.get('/unrestricted', function (a, n) {
+        this.body = 'ok';
+    }));
 
     it('200', (done) => {
         request(app.listen())
-            .get('/bob')
+            .get('/users/bob')
             .set('Authentication', authorize('admin:admin'))
-            .expect(200, done);
-    });
+            .expect(200)
+            .expect('bob', done)});            
 
-    it('403', (done) => {
-        request(app.listen())
-            .get('/bob')
-            .set('Authentication', authorize('bob:bob'))
-            .expect(403, done);
-    });
+        it('403', (done) => {
+            request(app.listen())
+                .get('/users/bob')
+                .set('Authentication', authorize('bob:bob'))
+                .expect(403, done);
+        });
 
-})
+        it('dmz:200', (done) => {
+            request(app.listen())
+                .get('/dmz')
+                .expect(200, done);
+        });
+
+        it('unrestricted:200', (done) => {
+            request(app.listen())
+                // route added after with no restrictions after auht:middleware => requires auth , does not requires 'role' 
+                .get('/unrestricted')
+                .set('Authentication', authorize('bob:bob'))
+                .expect(200, done);
+        });
+
+    })
+
+    type KoaNext = () => any
+    type KoaMiddleware = (ctx: Koa.Context, KoaNext) => any;
